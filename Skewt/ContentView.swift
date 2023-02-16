2ab23b24ea1628be25e259e6caa5255f228f9c53
Jason Neel <jason.neel@gmail.com>
2023-02-15 16:11:45 -0800

    Initial Commit

---
+++ Skewt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Skewt
+//
+//  Created by Jason Neel on 2/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Skewt/SkewtApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkewtApp.swift
+//  Skewt
+//
+//  Created by Jason Neel on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct SkewtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
