19851779ccd73a65d3bc8d74fd0aee7231e4881f
Jason Neel <jason.neel@gmail.com>
2023-05-18 12:36:39 -0700

    Initial work generating request URLs from sounding request

--- Skewt/SoundingRequest.swift
+++ Skewt/SoundingRequest.swift
@@ -7,7 +7,15 @@
 
 import Foundation
 
+fileprivate let locationFormatter = SoundingRequestLocationFormatter()
+
 struct SoundingRequest {
+    enum Location {
+        case name(String)
+        case geolocation(latitude: Double, longitude: Double)
+    }
+    
+    private static let rucUrl = "http://rucsoundings.noaa.gov/get_soundings.cgi"
     let location: Location
     let modelName: SoundingType?
     let startTime: Date?
@@ -27,7 +35,52 @@ struct SoundingRequest {
     }
 }
 
-enum Location {
-    case name(String)
-    case geolocation(latitude: Double, longitude: Double)
+class SoundingRequestLocationFormatter {
+    private var formatter: NumberFormatter
+    
+    init(numberOfDecimals: Int = 3) {
+        formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.minimumFractionDigits = numberOfDecimals
+        formatter.maximumFractionDigits = numberOfDecimals
+    }
+    
+    func string(forLocation location: SoundingRequest.Location) -> String {
+        switch location {
+        case .name(let name):
+            return name
+        case .geolocation(latitude: let latitude, longitude: let longitude):
+            let latitudeString = formatter.string(from: latitude as NSNumber)!
+            let longitudeString = formatter.string(from: longitude as NSNumber)!
+            return "\(latitudeString),\(longitudeString)"
+        }
+    }
+}
+
+extension SoundingRequest {
+    var url: URL {
+        var components = URLComponents(string: SoundingRequest.rucUrl)!
+        components.queryItems = [URLQueryItem(name: "airport",
+                                               value: locationFormatter.string(forLocation: location))]
+                                  
+        if let modelName = modelName {
+            components.queryItems!.append(URLQueryItem(name: "data_source", value: modelName.rawValue))
+        }
+        
+        if let startTime = startTime {
+            components.queryItems!.append(URLQueryItem(name: "startSecs",
+                                                       value: String(Int(startTime.timeIntervalSince1970))))
+        }
+        
+        if let endTime = endTime {
+            components.queryItems!.append(URLQueryItem(name: "endSecs",
+                                                       value: String(Int(endTime.timeIntervalSince1970))))
+        }
+        
+        if let numberOfHours = numberOfHours {
+            components.queryItems!.append(URLQueryItem(name: "n_hrs", value: String(Int(numberOfHours))))
+        }
+        
+        return components.url!
+    }
 }
