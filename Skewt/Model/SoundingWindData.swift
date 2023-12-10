3579feaed0850f7f0a228ffcc48d509d3cd94b8f
Jason Neel <jason.neel@gmail.com>
2023-12-10 13:14:26 -0800

    Remove wind shear data that was unused

--- Skewt/Model/SoundingWindData.swift
+++ Skewt/Model/SoundingWindData.swift
@@ -7,37 +7,6 @@
 
 import Foundation
 
-enum WindShearError: Error {
-    case missingWindData
-}
-
-struct WindShear {
-    let below: LevelDataPoint
-    let above: LevelDataPoint
-    
-    init(_ a: LevelDataPoint, _ b: LevelDataPoint) throws {
-        guard a.windDirection != nil, a.windSpeed != nil, b.windDirection != nil, b.windSpeed != nil else {
-            throw WindShearError.missingWindData
-        }
-        
-        below = a
-        above = b
-    }
-    
-    var shear: (n: Double, e: Double) {
-        let winds = [below.windComponents!, above.windComponents!]
-        
-        return (n: winds[1].n - winds[0].n, e: winds[1].e - winds[0].e)
-    }
-    
-    var shearPerAltitude: (n: Double, e: Double) {
-        let shear = self.shear
-        let dPressureAltitude: Altitude = .standardAltitude(forPressure: above.pressure) - .standardAltitude(forPressure: below.pressure)
-        
-        return (n: shear.n / dPressureAltitude, e: shear.e / dPressureAltitude)
-    }
-}
-
 extension LevelDataPoint {
     var windComponents: (n: Double, e: Double)? {
         guard let windSpeed = windSpeed, let windDirection = windDirection else {
@@ -60,14 +29,6 @@ extension Sounding {
     /// Wind data as north/east components
     var windDataDirectionalComponents: [(n: Double, e: Double)] {
         windData.map { $0.windComponents! }
-    }
-    
-    var windShearData: [WindShear] {
-        let windData = windData
-        
-        return stride(from: 0, to: windData.count - 1, by: 1).compactMap {
-            try? WindShear(windData[$0], windData[$0 + 1])
-        }
     }
 }
 
