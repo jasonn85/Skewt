03ac995aae358f7a166246b984cd19191b5cc94a
Jason Neel <jason.neel@gmail.com>
2023-12-12 13:39:26 -0800

    Remove some unneeded type guff

--- Skewt/State/Middleware/UpdateRAOBTimeMiddleware.swift
+++ Skewt/State/Middleware/UpdateRAOBTimeMiddleware.swift
@@ -12,12 +12,12 @@ extension Middlewares {
     /// This middleware detects if our current selection is most recent sounding and we just received a list of
     /// available soundings that shows the latest data is older than one sounding ago (so the request will return blank data)
     static let updateRaobTimeMiddleware: Middleware<SkewtState> = { state, action in
-        guard case RecentSoundingsState.Action.didReceiveList(let soundingList) = action,
-              case SoundingSelection.ModelType.raob = state.currentSoundingState.selection.type,
-              case SoundingSelection.Location.named(let locationName) = state.currentSoundingState.selection.location,
+        guard case .didReceiveList(let soundingList) = action as? RecentSoundingsState.Action,
+              case .raob = state.currentSoundingState.selection.type,
+              case .named(let locationName) = state.currentSoundingState.selection.location,
               let wmoId = LocationList.allLocations.locationNamed(locationName)?.wmoId,
               let mostRecentSounding = soundingList.lastSoundingTime(forWmoId: wmoId),
-              case SoundingSelection.Time.numberOfSoundingsAgo(let soundingsAgo) = mostRecentSounding.soundingSelectionTime(forModelType: .raob),
+              case .numberOfSoundingsAgo(let soundingsAgo) = mostRecentSounding.soundingSelectionTime(forModelType: .raob),
               soundingsAgo > 1
         else {
             return Empty().eraseToAnyPublisher()
