39eaec25d92b630a31c3a4097efb7dfdaf5378eb
Jason Neel <jason.neel@gmail.com>
2026-03-12 13:17:30 -0700

    Make the store a @StateObject to fix an issue where data would be blanked on app resume

--- Skewt/SkewtApp.swift
+++ Skewt/SkewtApp.swift
@@ -9,21 +9,21 @@ import SwiftUI
 
 @main
 struct SkewtApp: App {
+    @StateObject private var store = Store(
+        initial: SkewtState(),
+        reducer: SkewtState.reducer,
+        middlewares: [
+            Middlewares.locationMiddleware,
+            Middlewares.consoleLogger,
+            Middlewares.userDefaultsSaving,
+            Middlewares.openMeteoApi,
+            Middlewares.ncafSoundingMiddleware,
+            Middlewares.ncafSoundingData,
+            Middlewares.uwySoundingData
+        ]
+    )
+
     var body: some Scene {
-        let store = Store(
-            initial: SkewtState(),
-            reducer: SkewtState.reducer,
-            middlewares: [
-                Middlewares.locationMiddleware,
-                Middlewares.consoleLogger,
-                Middlewares.userDefaultsSaving,
-                Middlewares.openMeteoApi,
-                Middlewares.ncafSoundingMiddleware,
-                Middlewares.ncafSoundingData,
-                Middlewares.uwySoundingData
-            ]
-        )
-        
         WindowGroup {
             ContentView()
                 .environmentObject(store)
